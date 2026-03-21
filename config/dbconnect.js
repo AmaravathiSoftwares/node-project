3ac712b21bed0d682f6acb3b3694fbce6d6bf210
AmaravathiSoftwares <raviteja@amaravathisoftware.com>
2026-03-21 17:03:15 +0530

    db_worng

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,38 +1,16 @@
-import { createPool } from 'mysql2';
-var MySQLConPool = {};
+import { createPool } from "mysql2";
+import envs from "../config.js";
 
-// var USER = 'amaravathilive_users';
-// var PWD = 'bX+#IyoowOAd';
-// var DATABASE = 'amaravathilive_new_database';
-// var DB_HOST_NAME = 'localhost';
-
-
-// var USER = 'root';  
-// var PWD = '';
-// var DATABASE = 'tms_db2';
-// var DB_HOST_NAME = 'localhost';
-var USER = 'aneel';
-var PWD = 'Aneel@3699';
-var DATABASE = 'tms_db2';
-var DB_HOST_NAME = 'localhost';
-
-
-var MAX_POOL_SIZE = 100;
-
-var MySQLConPool = createPool({
-    host: DB_HOST_NAME,
-    port: 3306,
-    user: USER,
-    password: PWD,
-    database: DATABASE,
-    connectTimeout: 20000,
-    connectionLimit: MAX_POOL_SIZE,
-    debug: false,
-    multipleStatements: true
+const MySQLConPool = createPool({
+  host: envs.DB_HOST || "127.0.0.1",
+  port: Number(envs.DB_PORT || 3306),
+  user: envs.DB_USER,
+  password: envs.DB_PASSWORD,
+  database: envs.DB_NAME,
+  connectTimeout: 20000,
+  connectionLimit: Number(envs.DB_POOL || 100),
+  debug: false,
+  multipleStatements: true
 });
 
-
-const _MySQLConPool = MySQLConPool;
-export { _MySQLConPool as MySQLConPool };
-
-
+export { MySQLConPool };
